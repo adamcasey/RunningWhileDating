cf11fd8476fcada610ac31c64913a6f5b1f3416e
Adam <adam-casey@live.com>
2019-08-09 18:13:10 -0600

    Work on nodemon errors in app.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import FormContainer from "./js/components/container/FormContainer.jsx";
 require ("./server.js")
 require("./style.css")
-var express = require('express');
-
-// var index = require('./routes/index');
-// var users = require('./routes/users');
-
-var app = express();
 alert("Yes!")
