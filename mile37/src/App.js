da4bb4bcd257eb2363d70750feb5f2fe4bdac82b
adamcasey <adam-casey@live.com>
2019-08-19 20:28:02 -0600

    Init create-react-app and add background image

---
+++ mile37/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import logo from './logo.svg';
+import logo from './running.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        Mile 37
+      </header>
+    </div>
+  );
+}
+
+export default App;
