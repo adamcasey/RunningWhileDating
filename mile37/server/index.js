005e0c63011e92aa654867b062e27d1a378e2ab8
adamcasey <adam-casey@live.com>
2019-09-04 19:50:28 -0600

    Submit errors on StackOverflow
    /home/masonmontero/Desktop/RunningWhileDating/mile37/node_modules/passport/lib/middleware/authenticate.js:353
    [1]           return callback(err);
    [1]                  ^
    [1]
    [1] TypeError: callback is not a function
    [1]     at Strategy.strategy.error (/home/masonmontero/Desktop/RunningWhileDating/mile37/node_modules/passport/lib/middleware/authenticate.js:353:18)
    [1]     at /home/masonmontero/Desktop/RunningWhileDating/mile37/node_modules/passport-oauth/lib/passport-oauth/strategies/oauth2.js:125:32
    [1]     at /home/masonmontero/Desktop/RunningWhileDating/mile37/node_modules/oauth/lib/oauth2.js:191:18
    [1]     at passBackControl (/home/masonmontero/Desktop/RunningWhileDating/mile37/node_modules/oauth/lib/oauth2.js:132:9)
    [1]     at IncomingMessage.<anonymous> (/home/masonmontero/Desktop/RunningWhileDating/mile37/node_modules/oauth/lib/oauth2.js:157:7)
    [1]     at IncomingMessage.emit (events.js:203:15)
    [1]     at endReadableNT (_stream_readable.js:1145:12)
    [1]     at process._tickCallback (internal/process/next_tick.js:63:19)
    
    Error occured while trying to proxy to: 127.0.0.1:3000/auth/strava/callback?state=&code=164fce02d15f55111824795821549afc8f1293f6&scope=read

--- mile37/server/index.js
+++ mile37/server/index.js
@@ -24,7 +24,7 @@ passport.use(new StravaStrategy({
   clientID: keys.STRAVA.clientID,
   clientSecret: keys.STRAVA.clientSecret,
   // What will it do after verifying user with log-in credentials
-  callbackURL: "http://127.0.0.1:5000/auth/strava/callback"
+  callbackURL: "http://127.0.0.1:3000/auth/strava/callback"
   //callbackURL: "/auth/strava/callback"
 },
                                 
@@ -33,57 +33,30 @@ passport.use(new StravaStrategy({
   (accessToken, refreshToken, profile, cb) => {
     console.log(chalk.blue(JSON.stringify(profile)));
     // put all of the key:value pairs from the profile into a 'user' object
-    user = { ...profile };
+    // user = { ...profile };
 
-    User.findOrCreate({ stravaId: profile.id }, (err, user) => {
-      return cb(err, user);
+    // User.findOrCreate({ stravaId: user.id }, (err, user) => {
+    //   return cb(err, user);
+    // });
+    process.nextTick(function () {
+        // To keep the example simple, the user's Strava profile is returned to
+        // represent the logged-in user. In a typical application, you would want
+        // to associate the Strava account with a user record in your database,
+        // and return that user instead.
+        return done(null, profile);
     });
     //return cb(null, profile);
   }));
-  
-                                
-  /*
- // copied from passportjs.org/packages/passport-strava                                
-  function(accessToken, refreshToken, profile, cb) {
-  
     
-    // Try uncommenting this block if still getting InternalOauth Errors
-        process.nextTick(function () {
-            // To keep the example simple, the user's Strava profile is returned to
-            // represent the logged-in user. In a typical application, you would want
-            // to associate the Strava account with a user record in your database,
-            // and return that user instead.
-            return done(null, profile);
-        });
-  
-    // User.findOrCreate({ stravaId: profile.id }, (err, user) => {
-    //   return cb(err, user);
-    // });
-  }
-));
-*/
-  
 // setup the server
 const app = express();
 // cors will alow us to make requests to Strava
 app.use(cors());
 app.use(passport.initialize());
 
-/*
-// define routes 
-// call from front-end to login user with Strava. Will call everything up above
-app.get("/auth/strava", passport.authenticate( strategy: "strava"));
-// define callback
-app.get("/auth/strava/callback", 
-  passport.authenticate(("strava"),
-    options: (req, res) => {
-      res.redirect("/profile");
-     }));
-*/
-
 // Alternate implementation
 // call from front-end to login user with Strava. Will call everything up above
-app.get("/auth/strava", passport.authenticate("strava", { failureRedirect: '/login' }, {failWithError: true}));
+app.get("/auth/strava", passport.authenticate("strava", { scope: ['read'] }));
 // define callback
 app.get("/auth/strava/callback", 
   passport.authenticate("strava", { failureRedirect: '/login' }, {failWithError: true}),
