df99a60c554f510fb3f47d94735727f07d6754af
adamcasey <adam-casey@live.com>
2019-11-16 19:00:08 -0700

    Add styling to text on profile page. No access to photos

--- src/components/displays/CalculateSpeed.jsx
+++ src/components/displays/CalculateSpeed.jsx
@@ -4,17 +4,45 @@ import UserProvider from "../../contexts/UserProvider";
 
 const CalculateSpeed = () => {
 
+    const pStyle = {
+        color: "red",
+        textAlign: "center",
+        paddingTop: "50px",
+        paddingLeft: "50px",
+        whiteSpace: "pre",
+        fontFamily: "Papyrus",
+        position: "absolute",
+        margin: 2,
+        fontSize: 30
+    };
+
     const userData = useContext(UserProvider.context);
 
-    //const [accessToken, setAccessToken] = useState(props.accessToken);
-    //const text = _.isEmpty(userData) ? LoginMsg : "How fast are you...";
+    console.log({userData});
+
+    let accessToken = userData.token;
+
+    var userInfo = Object.keys(userData).map(function(s){ return userData[s] });
+    //console.log({userInfo});
+
+    var userName = userInfo[2];
+    //console.log({userName});
+
+    var userPhotos = userInfo[4];
+    var profilePhoto = {userPhotos}[0];
+    console.log({userPhotos});
+    console.log({profilePhoto});
+
 
     return (
-       <div style={{ fontSize: 20, color: "red", textAlign: "center", whiteSpace: "pre", position: "absolute"}}>
-          Accesstoken should be here {'\n'}
-          { userData.token } {'\n'}
-          But it isnt...
-      </div>
+
+       <div style={pStyle}>
+          User Name: {" "}
+          { userName } {'\n'}
+          Accesstoken: {" "}
+          { accessToken } {'\n'}
+          Photo: 
+       </div>
     );
 };
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,19 +1,12 @@
 import React, { useContext, useState } from "react";
 import UserProvider from "../contexts/UserProvider";
-// import Terminal from "../components/displays/Terminal";
-// import Col from "../components/wrappers/Col";
-// import DataTags from "../components/menus/DataTags";
 import CalculateSpeed from "../components/displays/CalculateSpeed";
 import _ from "lodash";
-
-//const LoginMsg = "Please login to your Strava account";
-
 
 class Profile extends React.Component {
     
     //const text = _.isEmpty(userData) ? LoginMsg : "How fast are you...";
     render() {
-        //const userData = useContext(UserProvider.context);
         return (
             <div className="page">
                 <CalculateSpeed />
